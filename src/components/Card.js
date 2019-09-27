c213a286a7d198520619c66d61582eafc51c600e
Sayan Mukherjee <hello@sayanmukherjee.com>
2019-09-27 19:33:44 +0530

    updating files

--- src/components/Card.js
+++ src/components/Card.js
@@ -17,36 +17,14 @@ class Card extends Component {
       .then(species => this.setState({ species: species.name }))
 
     //films
-    // const filmsArray = [];
-   
+    const filmsArray = [];
+    const fetchFilms = this.props.films.map((filmUrl, i) => { fetch(filmUrl) 
+    	.then(response => response.json())
+    	.then(data => filmsArray.push(data.title)
+    	)
+    })
 
-    // const fetchFilms = this.props.films.map((filmUrl, i) => { fetch(filmUrl) 
-    // 	.then(response => response.json())
-    // 	.then(data => {
-    // 		return filmsArray.push(data.title)
-    // 		// filmsArray.push(`${data.episode_id} - ${data.title}`)
-    // 		// filmsArray.sort();
-    // 	})
-    // })
-     
-
-    // Promise.all(fetchFilms)
-    // 	.then(promisesArray => {this.setState({films: filmsArray}) 
-    // 		console.log(filmsArray);})
-    // 	.catch(err => console.log(err))
-
-
-
-    // fetch(this.props.films)
-    // 	.then(response => response.json())
-    // 	.then(data => this.setState({films: data.title}))
-
- //    Promise.all(filmsArray.map(url => {
-	// return fetch(url).then(response => response.json)
-	// 	})).then(data => {
-	// 		this.setState({films:data.title})
-	// 	}).catch (error => console.log('error'));
-  	// }
+    console.log(filmsArray)
 }
   render(){ 
   	const { species, films, data } = this.state;
@@ -56,7 +34,7 @@ class Card extends Component {
 	      		<h2>{this.props.name}</h2>
 	      		<h4>{species}</h4>
 	      		<ul className="ul-style">
-              		{this.props.films.map((film, i) => (
+              		{films.map((film, i) => (
 	                <li key={i}>
 	   					{film}
 	                </li>
