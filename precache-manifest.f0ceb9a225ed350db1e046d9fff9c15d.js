4a0744f2a7f49328c70ae2b803b9cd3f77222281
Sayan Mukherjee <hello@sayanmukherjee.com>
2019-11-07 10:18:33 +0530

    Updates

---
+++ precache-manifest.f0ceb9a225ed350db1e046d9fff9c15d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf928a20ecdac01c6a6958a0293f9ce",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "01344fcc30af37bb2548",
+    "url": "/starwars/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ba67cfe293b1b53e0a30",
+    "url": "/starwars/static/css/main.4640db43.chunk.css"
+  },
+  {
+    "revision": "01344fcc30af37bb2548",
+    "url": "/starwars/static/js/2.8259b7c7.chunk.js"
+  },
+  {
+    "revision": "ba67cfe293b1b53e0a30",
+    "url": "/starwars/static/js/main.cdc6b662.chunk.js"
+  },
+  {
+    "revision": "a98754e8c562609ecb0b",
+    "url": "/starwars/static/js/runtime-main.599f438f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/starwars/static/media/SEGA.12a3db8b.woff"
+  }
+]);
